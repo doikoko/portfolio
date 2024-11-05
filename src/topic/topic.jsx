335ff428203c4def10df36de639026e0c08e0cae
doikoko <danilsasunov.2089@gmail.com>
2024-11-05 21:49:07 +0300

    v 1.3: almost ready modal window and finish develop main page

--- src/topic/topic.jsx
+++ src/topic/topic.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ChangeText from "./changeText";
 import Modal from "./modal";
+import gmailIcon from "../img/gmail.png"
+import telegramIcon from "../img/telegram.png"
+import phoneIcon from "../img/telephone.png"
 
 class Topic extends React.Component{
     constructor(props) {
         super(props);
+        this.state ={
+            modalIsActive: false,
+            modalNumber: 0
+        }
         this.links = {
             telegram: "https://t.me/DaniilSasunov",
             gmail: "danilsasunov.2089@gmail.com",
@@ -18,12 +25,12 @@ class Topic extends React.Component{
             <figure id={'photoOval'}></figure>
             <ChangeText/>
             <hr/>
-            <ul>
-                <li onClick={() => Modal(1)}>tgIcon</li>
-                <li onClick={() => Modal(2)}>gmIcon</li>
-                <li onClick={() => Modal(3)}>PhIcon</li>
-            </ul>
-            <div id="modelMain"></div>
+            <div id={'contacts'}>
+                <img src={telegramIcon} alt={"telegram"} onClick={() => this.setState({modalIsActive: true, modalNumber: 1})}></img>
+                <img src={gmailIcon} alt={"gmail"} onClick={() => this.setState({modalIsActive: true, modalNumber: 2})}></img>
+                <img src={phoneIcon} alt={"phone"} onClick={() => this.setState({modalIsActive: true, modalNumber: 3})}></img>
+            </div>
+            <Modal isActive={this.state.modalIsActive} number={this.state.modalNumber}/>
         </header>
     }
 }
